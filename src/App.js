fe6435efa523e4116f7fba081656ae7ede848da2
Enoch Wong <enwong10@gmail.com>
2020-07-15 12:32:43 -0400

    Init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor() {
+    super();
 
+    this.state = {
+      file: null,
+      audio: null,
+      errorMessage: false
+    }
+    this.dropRef = React.createRef()
+
+    this.startAudio = this.startAudio.bind(this);
+    this.stopAudio = this.stopAudio.bind(this);
+  }
+
+  componentDidMount() {
+    let div = this.dropRef.current
+    div.addEventListener('dragover', this.handleDrag)
+    div.addEventListener('drop', this.handleDrop)
+  }
+
+  componentWillUnmount() {
+    let div = this.dropRef.current
+    div.removeEventListener('dragover', this.handleDrag)
+    div.removeEventListener('drop', this.handleDrop)
+  }
+  handleDrag = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+  }
+  handleDrop = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+    console.log(e.dataTransfer.files);
+    if (e.dataTransfer.files && e.dataTransfer.files.length === 1 && e.dataTransfer.files[0].type === "audio/mpeg") {
+      this.setState({ file: e.dataTransfer.files[0] }, () => {
+        this.setState({ audio: new Audio(URL.createObjectURL(this.state.file)) });
+      });
+      this.setState({ errorMessage: false });
+    }
+    else {
+      this.setState({ errorMessage: true });
+    }
+  }
+
+  startAudio() {
+    this.state.audio.play();
+  }
+
+  stopAudio() {
+    this.state.audio.pause();
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <div className="popup">
+            <h2 style={{ color: "black", textAlign: "left", marginLeft: "0.5em" }}>Import:</h2>
+            <div className="drop-inner" ref={this.dropRef}><h4 style={{ color: "darkcyan" }}>Drop audio file here!</h4></div>
+
+            <div style={{display: "flex", flexDirection: "column", flex: "1", textAlign: "left"}}>
+              <label className={!this.state.errorMessage ? 'hidden' : ''} style={{ color: "red"}}>Error: File not supported. Please import .mp3 files only.</label>
+              <label className={this.state.file === null ? 'hidden' : ''}>Current File: {this.state.file === null ? '' : this.state.file.name}</label>
+            </div><div style={{ display: "flex", justifyContent: "space-between", margin: "10px" }}>
+              <div>
+                <button onClick={this.startAudio} style={{ marginRight: "10px" }}>Play</button>
+                <button onClick={this.stopAudio}>Stop</button>
+                <div className='audio-error' style={{ display: "none" }}>Audio is not supported in this browser.</div>
+              </div>
+              <div style={{ display: "flex", flexDirection: "row", justifyContent: "flex-end" }}>
+                <button style={{ backgroundColor: "green", marginRight: "10px", marginLeft: "10px" }}>Save</button>
+                <button style={{ backgroundColor: "red" }}>Cancel</button>
+              </div>
+            </div>
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
 export default App;
